089082a261291655430ae7bf1550aa15d49831c0
Lariba Cloud <stecouto@proton.me>
2026-08-11 01:19:59 +0100

    feat: use MORABEZA player state in game mode

--- Source/MORABEZA/MORABEZAGameMode.cpp
+++ Source/MORABEZA/MORABEZAGameMode.cpp
@@ -1,7 +1,9 @@
 #include "MORABEZAGameMode.h"
 #include "MORABEZACharacter.h"
+#include "MORABEZAPlayerState.h"
 
 AMORABEZAGameMode::AMORABEZAGameMode()
 {
     DefaultPawnClass = AMORABEZACharacter::StaticClass();
+    PlayerStateClass = AMORABEZAPlayerState::StaticClass();
 }
